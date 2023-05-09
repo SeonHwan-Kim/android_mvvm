afd1977e93320c9511c4aff19339626bdc45f27f
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-10 02:58:54 +0900

    feat : viewModel 사용 #1

--- app/src/main/java/org/seonhwan/android/mvvmstudy/presentation/main/MainActivity.kt
+++ app/src/main/java/org/seonhwan/android/mvvmstudy/presentation/main/MainActivity.kt
@@ -1,26 +1,29 @@
-package org.seonhwan.android.mvvmstudy
+package org.seonhwan.android.mvvmstudy.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import org.seonhwan.android.mvvmstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var counter = 100
+    private val factory = MainFactory(100)
+    private val viewModel by viewModels<MainViewModel>() { factory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvMainCounter.text = counter.toString()
+        binding.tvMainCounter.text = viewModel.counter.toString()
         this.onClickIncreaseBtn()
     }
 
     private fun onClickIncreaseBtn() {
         with(binding) {
             btMainIncrease.setOnClickListener {
-                counter += 1
-                tvMainCounter.text = counter.toString()
+                viewModel.counter += 1
+                tvMainCounter.text = viewModel.counter.toString()
             }
         }
     }
