2e85734df0928b295162eedd0233230a51397c79
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-10 02:57:46 +0900

    add : MainViewModel 생성 #1

---
+++ app/src/main/java/org/seonhwan/android/mvvmstudy/presentation/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package org.seonhwan.android.mvvmstudy.presentation.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(_counter: Int) : ViewModel() {
+    var counter: Int = _counter
+}
